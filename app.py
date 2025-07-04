252f37a559ee45d4c981118c4e1dba4224eaab49
Dharshini-7v <vdharshini206@gmail.com>
2025-07-04 07:30:13 -0700

    updated

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from chatbot import get_best_match
 
-st.set_page_config(page_title="FAQ Chatbot", page_icon="🤖")
-st.title("💬 FAQ Troubleshooting Chatbot")
+st.set_page_config(page_title="FAQ Bot", page_icon="💻")
+st.title("💬 Laptop Troubleshooting Chatbot")
 
-st.write("Ask a question about your laptop issue:")
+st.write("Ask a question related to your laptop issues:")
 
-user_input = st.text_input("🗣️ Your Question")
+user_input = st.text_input("🔍 Your Question")
 
 if user_input:
     response = get_best_match(user_input)
-    st.success(f"🤖 Bot: {response}")
+    st.success(f"🤖 {response}")
+
