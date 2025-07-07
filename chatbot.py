cb7a8919ea30d53aa6eff2a9230f400d3fc356ba
Dharshini-7v <vdharshini206@gmail.com>
2025-07-07 04:13:53 -0700

    updated

--- chatbot.py
+++ chatbot.py
@@ -8,8 +8,10 @@ import re
 
 # Download necessary NLTK data (run this once
 nltk.download('stopwords')
+nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+
 
 # --- Text Preprocessing Functions ---
 lemmatizer = WordNetLemmatizer()
